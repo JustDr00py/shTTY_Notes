41f99c048769dbd6522fed0c7271c81049517b82
JustDr00py <115050378+JustDr00py@users.noreply.github.com>
2022-11-13 18:12:15 -0800

    Update notes.py

--- notes.py
+++ notes.py
@@ -1,15 +1,16 @@
 import os
 from cryptography.fernet import Fernet
-import os
+from os.path import exists
 from time import sleep
 
-path = "./notes"
-dir_list = os.listdir(path)
+
 # prints all files
 
+#q = ""
 nn = ['1','new', 'New', 'New Note', 'new note', 'n', 'nn']
 on = ['2','open', 'Open', 'Open Note', 'open note', 'o', 'r']
 gk = ['3', 'key', 'gk', 'get key', 'get']
+path = "./notes"
 
 class Note:
     def __init__(self, title, msg):
@@ -20,8 +21,13 @@ class Note:
         m1 = input('Title: ')
         m2 = input('Note: ')
         print(m2)
+
         note = Note(m1,m2)
+        #print(f'Name: \n {m1}')
+        #print(f'Note: \n {m2}')
+
         print(f'{note.title} \n {note.msg}')
+
         f = open(f'./notes/{note.title}.txt', 'xt')
         f.write(f'{note.msg}')
         f.close()
@@ -38,6 +44,7 @@ class Note:
         return answer
 
     def delete(x):
+        #delete = input('File to Delete: ')
         if os.path.exists(f'./notes/{x}.txt'):
             os.remove(f'./notes/{x}.txt')
         else:
@@ -45,6 +52,7 @@ class Note:
             
 
     def encrypt(x):
+        #answer = input('Select a File: ')
         # opening the key
         with open('mykey.key', 'rb') as filekey:
             key = filekey.read()
@@ -65,6 +73,7 @@ class Note:
             encrypted_file.write(encrypted)
 
     def decrypt(x):
+        #answer = input('Select a File: ')
         # opening the key
         with open('mykey.key', 'rb') as filekey:
             key = filekey.read()
@@ -98,6 +107,28 @@ class Note:
                     key = Fernet.generate_key() #this is your "password"
                     with open('mykey.key', 'wb') as mykey:
                         mykey.write(key)
+        else:
+            key = Fernet.generate_key() #this is your "password"
+            with open('mykey.key', 'wb') as mykey:
+                mykey.write(key)
+                print('keyfile created!')
+    def start():
+        if exists('mykey.key'):
+            print('keyfile found!')
+            sleep(.3)
+            print('Checking Folders...')
+            sleep(.3)
+            if not os.path.exists('notes'):
+                print('Making notes directory...')
+                sleep(.3)
+                os.makedirs('./notes')
+            else:
+                print('notes folder exists!')
+                global dir_list
+                dir_list = os.listdir(path)
+
+        else:
+            Note.get_key()
 
     def menu():
         print(' ________________')
